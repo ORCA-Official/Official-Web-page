e0a247b4ee2efb3a2fa188a991118971fff439be
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-10-08 13:09:18 +0530

    page three is created

--- src/view/page/About.js
+++ src/view/page/About.js
@@ -66,7 +66,29 @@ const About = () => {
                     </div>
                 </div>
             </div>
+            <div className="min-h-screen flex flex-col items-center justify-center relative">
+                {/* Background Image */}
+                <div className="absolute inset-0">
+                    <img
+                        src={images}
+                        alt="Background Image"
+                        className="w-full h-full object-cover opacity-100"
+                    />
+                </div>
 
+                {/* Content */}
+                <div className="relative z-6 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                    <h1 className="text-9xl font-extrabold opacity-15">
+                        WHO WE ARE
+                    </h1>
+                </div>
+
+                <div className="absolute flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                    <h2 className="text-4xl font-extrabold">
+                        ABOUT US
+                    </h2>
+                </div>
+            </div>
         </>
     )
 }
