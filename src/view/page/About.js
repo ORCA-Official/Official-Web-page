3c08953335a506020b86602ef6bdcd0ba6f1173c
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-10-08 13:54:27 +0530

    update the h1,h2 tags in the all pages

--- src/view/page/About.js
+++ src/view/page/About.js
@@ -3,29 +3,7 @@ import images from "../../images/aboutBG.png";
 const About = () => {
     return(
         <>
-        {/*<div className="relative h-screen w-full bg-black">
-            <div className="absolute inset-0">
-                <img
-                    src={images}
-                    alt="Background Image"
-                    className="w-full h-full object-cover opacity-100"
-                />
-
-                <div className="relative flex flex-col items-center justify-center h-full text-center text-gray-800 space-y-4">
-                    <h1 className="text-9xl font-extrabold opacity-20">
-                        WHO WE ARE
-                    </h1>
-                </div>
-
-                <div className="relative flex flex-col items-center justify-center h-full text-center text-gray-800 space-y-4">
-                    <h2 className="text-4xl font-extrabold">
-                        About us
-                    </h2>
-                </div>
-
-            </div>
-        </div>*/}
-
+            {/*----------------------------- Page 1 -----------------------------*/}
             <div className="min-h-screen flex flex-col items-center justify-center relative">
                 {/* Background Image */}
                 <div className="absolute inset-0">
@@ -37,35 +15,42 @@ const About = () => {
                 </div>
 
                 {/* Content */}
-                <div className="relative z-6 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                <div className="relative -mt-80 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
                     <h1 className="text-9xl font-extrabold opacity-15">
                         WHO WE ARE
                     </h1>
                 </div>
 
-                <div className="absolute flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                <div className="absolute -mt-80 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
                     <h2 className="text-4xl font-extrabold">
                         ABOUT US
                     </h2>
                 </div>
-            </div>
 
+                <div>
+
+                </div>
+            </div>
+            {/*----------------------------- Page 2 -----------------------------*/}
             <div>
                 <div className="min-h-screen flex flex-col items-center justify-center relative bg-white">
                     {/* Content */}
-                    <div className="relative z-6 flex flex-col items-center justify-center h-full text-center text-gray-600 space-y-4">
+                    <div className="relative -mt-80 flex flex-col items-center justify-center h-full text-center text-gray-600 space-y-4">
                         <h1 className="text-9xl font-extrabold opacity-15">
                             WHAT WE DO
                         </h1>
                     </div>
 
-                    <div className="absolute flex flex-col items-center justify-center h-full text-center text-black space-y-4">
+                    <div className="absolute -mt-80 flex flex-col items-center justify-center h-full text-center text-black space-y-4">
                         <h2 className="text-4xl font-extrabold">
                             OUR SERVICES
                         </h2>
                     </div>
                 </div>
             </div>
+
+            {/*----------------------------- Page 3 -----------------------------*/}
+
             <div className="min-h-screen flex flex-col items-center justify-center relative">
                 {/* Background Image */}
                 <div className="absolute inset-0">
@@ -77,15 +62,15 @@ const About = () => {
                 </div>
 
                 {/* Content */}
-                <div className="relative z-6 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                <div className="relative -mt-80 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
                     <h1 className="text-9xl font-extrabold opacity-15">
-                        WHO WE ARE
+                        MINDS BEHIND
                     </h1>
                 </div>
 
-                <div className="absolute flex flex-col items-center justify-center h-full text-center text-white space-y-4">
+                <div className="absolute -mt-80 flex flex-col items-center justify-center h-full text-center text-white space-y-4">
                     <h2 className="text-4xl font-extrabold">
-                        ABOUT US
+                        OUR TEAM
                     </h2>
                 </div>
             </div>
