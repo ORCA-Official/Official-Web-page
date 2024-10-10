745e553958a969250bbbbd2c74315d21c91c1f30
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-10-10 23:35:40 +0530

    About Us What We Do Edited

---
+++ src/components/about/WhatWeDo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PageTitles from "../core/PageTitles";
+const WhatWeDo = () => {
+    return (
+        <div>
+            <PageTitles
+                dark={false}            // Use `true` if you want the dark mode
+                title="OUR SERVICES"
+                bgtitle="what we do"
+            />
+        </div>
+    )
+}
+
+export default WhatWeDo;
--- src/view/page/About.js
+++ src/view/page/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageTitles from "../../components/core/PageTitles"
 import WhoWeAre from "../../components/about/WhoWeAre";
+import WhatWeDo from "../../components/about/WhatWeDo";
 
 const About = () => {
     return (
         <div className="w-full">
             <WhoWeAre/>
+            <WhatWeDo/>
         </div>
     )
 }
