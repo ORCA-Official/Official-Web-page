013855c7f4b3f561acf52ef729c5545741cea269
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-10-10 20:54:56 +0530

    added services

---
+++ src/components/core/Services.js
@@ -0,0 +1,68 @@
+// ** React Imports
+import React, {useState} from "react";
+
+// ** React Icon Imports
+import { BsArrowDownRight } from "react-icons/bs";
+
+// ** Service Data Imports
+import {data} from '../../utils/services'
+
+const Services = () => {
+    return (
+        <div className={'w-full'}>
+            <div className={'flex flex-wrap justify-between'}>
+                {data.map((service, index) => (
+                    <ServiceCard service={service} index={index} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Services
+
+
+const ServiceCard = ({service, index}) => {
+
+    const [clicked, setClicked] = useState(false);
+
+    const expand = () => {
+        setClicked(!clicked)
+    }
+
+    const arrowStyle = {
+        transform: clicked ? 'rotate(45deg)' : 'rotate(0deg)',
+        transition: 'transform 0.3s ease-in-out'
+    };
+
+    return (
+        <div onClick={expand} className={`transition-all duration-300 ease-in-out h-max md:w-full w-[45%] flex-grow-0 border-b border-gray-400 
+            ${index < 1 ? 'border-y' : ''} 
+            ${clicked ? 'max-h-96 opacity-100' : ' max-h-12 overflow-hidden'} cursor-pointer`}
+        >
+            <div className={'w-full h-full'}>
+                <div className={`w-full flex justify-between font-Lato items-center p-2 ${clicked ? '' : ''}`}>
+                    <div className={'text-xl'}>
+                        <span className={'text-lg'}>{(index + 1).toString().padStart(2, '0')}. </span>
+                        <span>{service.name}</span>
+                    </div>
+
+                    <BsArrowDownRight style={arrowStyle} />
+                </div>
+
+                <div className={`pl-8 py-4 transition-all duration-300 ease-in-out ${clicked ? 'opacity-100' : 'opacity-0'}`}>
+                    <div className={'mt-1 flex flex-wrap gap-2'}>
+                        {service.stack.map((text) => (
+                            <span className={'px-1 py-0.5 border rounded-md border-black'}>{text}</span>
+                        ))}
+                    </div>
+
+                    <div className={'pt-4 pb-2'}>
+                        <p>{service.details}</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
