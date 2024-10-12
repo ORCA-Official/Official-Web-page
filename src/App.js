65380ef91d7903b61b6c4735e1432e1f5403ccf3
ORCA-Official <ishandanu999@gmail.com>
2024-10-12 15:50:24 +0530

    Contact Page Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from "./view/page/Home";
+import Contact from "./components/Contact";
 
 function App() {
 
 
     return (
         <div>
-            <Home />
+            {/*<Home />*/}
+            <Contact/>
         </div>
     );
 }
