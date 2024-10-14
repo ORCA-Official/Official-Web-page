cac2ae03404a2a64f514574bb34c43f1b093a601
DushanMalind <dushanmalinda010@gmail.com>
2024-10-14 19:22:53 +0530

    projectDetails Update

--- src/utils/projectDetails.js
+++ src/utils/projectDetails.js
@@ -7,8 +7,10 @@ export const projectDetails = [
             "cars, view details, and manage bookings with admin handling car listings and bookings.",
         githubLink: "https://github.com/your-car-rental-repo",
         liveLink: "https://car-rental-live.com",
-        backgroundImage: require('../assets/images/carbackground01.png'),
+        backgroundImage: require('../assets/images/carRentel.png'),
+        links:false
     },
+
     {
         title: "My Blog App",
         stack: ["Next Js", "CSS", "JavaScript", "TailwindCSS", "Google API", "MongoDB", "Firebase"],
@@ -17,7 +19,8 @@ export const projectDetails = [
             "Google APIs. Powered by Prisma ORM for efficient data management.",
         githubLink: "https://github.com/your-car-rental-repo",
         liveLink: "https://car-rental-live.com",
-        backgroundImage: require('../assets/images/blogapp.png'),
+        backgroundImage: require('../assets/images/myblogApp.png'),
+        links:false
     },
 
     {
@@ -27,7 +30,33 @@ export const projectDetails = [
             "upgrade selections, while admins manage everything via a comprehensive dashboard.",
         githubLink: "https://github.com/your-car-rental-repo",
         liveLink: "https://car-rental-live.com",
-        backgroundImage: require('../assets/images/lms.png'),
+        backgroundImage: require('../assets/images/travel.png'),
+        links:false
     },
+
+    {
+        title: "LMS System",
+        stack: ["React", "TailwindCSS", "JavaScript", "JWT", "Node.js", "MongoDB"],
+        description: "A learning management system (LMS) is a type of eLearning software designed to help you create," +
+            " manage, deliver, and assess training courses and L&D programs. These programs are conducted online, " +
+            "although they can be accessed offline from mobile devices with downloaded content",
+        githubLink: "https://github.com/your-car-rental-repo",
+        liveLink: "https://car-rental-live.com",
+        backgroundImage: require('../assets/images/LMS.png'),
+        links:false
+    },
+
+    {
+        title: "COINZEN - Finance Tracker",
+        stack: ["React Native", "Expo", "JavaScript", "NativeWind", "Firebase"],
+        description: "COINZEN is a comprehensive finance tracking app built with React Native and Expo. It helps users" +
+            " track expenses, set monthly budgets, and view categorized expense reports. The app features seamless " +
+            "Firebase integration for real-time data synchronization, secure user authentication, and a responsive UI" +
+            " with NativeWind for styling. Integrated with Google APIs for enhanced functionality.",
+        githubLink: "https://github.com/your-coinzen-repo",
+        liveLink: "https://coinzen-live.com/",
+        backgroundImage: require('../assets/images/coin.png'),
+        links:false
+    }
 
 ]
