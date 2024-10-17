a5f1da710dda157d6060f890314736f5901412af
DushanMalind <dushanmalinda010@gmail.com>
2024-10-17 13:09:16 +0530

    handleLoad

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { useEffect, useState } from 'react';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from "./view/Home/About";
 import Home from "./view/Home/Home";
 import Project from "./view/project/Project";
 import Contact from "./components/home/Contact";
 import Loader from "./components/core/Loader";
+import {useEffect, useState} from "react";
+
 
 function App() {
-    // Correct use of useState outside of useEffect
-    const [loading, setLoading] = useState(true);
+
+    const [isLoading, setIsLoading] = useState(true); // Add loading state
 
     useEffect(() => {
-        // Simulate an API call or data loading
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 3000); // 3 seconds to show full animation
+        const handleLoad = () => {
+            setIsLoading(false);
+        };
+        window.addEventListener('load', handleLoad);
 
-        // Clean up the timer when the component unmounts
-        return () => clearTimeout(timer);
-    }, []); // Empty dependency array to run effect once when the component mounts
+        return () => {
+            window.removeEventListener('load', handleLoad);
+        };
+    }, []);
 
     return (
         <>
-            {loading ? (
-                <Loader/>
+            {isLoading ? (
+                <Loader />
             ) : (
                 <BrowserRouter>
                     <Routes>
