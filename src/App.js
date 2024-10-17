5a25d6024ecb3594d39cdf4d306128f89263b574
ORCA-Official <ishandanu999@gmail.com>
2024-10-17 12:44:31 +0530

    Loader Content Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from "./view/Home/About";
 import Home from "./view/Home/Home";
 import Project from "./view/project/Project";
 import Contact from "./components/home/Contact";
+import Loader from "./components/core/Loader";
 
 function App() {
+    // Correct use of useState outside of useEffect
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        // Simulate an API call or data loading
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 3000); // 3 seconds to show full animation
+
+        // Clean up the timer when the component unmounts
+        return () => clearTimeout(timer);
+    }, []); // Empty dependency array to run effect once when the component mounts
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/project" element={<Project/>} />
-                <Route path="/contact" element={<Contact/>} />
-                <Route path="/*" element={<Home/>} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            {loading ? (
+                <Loader/>
+            ) : (
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/about" element={<About/>}/>
+                        <Route path="/project" element={<Project/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
+                        <Route path="/*" element={<Home/>}/>
+                    </Routes>
+                </BrowserRouter>
+            )}
+        </>
     );
 }
 
